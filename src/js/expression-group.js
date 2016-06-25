8e6481275a83b5e7b5a8a06b70a3e33cc5794d83
Russell Cousineau <russell.miller@rentrakmail.com>
2016-06-24 20:51:30 -0700

    add group component

---
+++ src/js/expression-group.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('ngEquation')
+    .controller('ExpressionGroupCtrl', function() {
+        var ctrl = this;
+
+        console.log(ctrl.operator, ctrl.operands);
+    })
+    .directive('expressionGroup', function($templateCache) {
+        return {
+            restrict: 'EA',
+            scope: {},
+            bindToController: {
+                containerClass: '@',
+                operator: '@',
+                operands: '='
+            },
+            controller: 'ExpressionGroupCtrl',
+            controllerAs: 'group',
+            template: $templateCache.get('expression-group.html')
+        };
+    });
