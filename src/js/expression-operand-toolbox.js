1e2a756d37c9532b94b105fe56991c97b2e733c5
Russell Cousineau <russell.miller@rentrakmail.com>
2016-07-08 11:38:47 -0700

    remove toolbox `OperandOptions`, add `removeOperand` method to toolbox

--- src/js/expression-operand-toolbox.js
+++ src/js/expression-operand-toolbox.js
@@ -4,19 +4,6 @@ angular.module('ngEquation')
     .controller('ExpressionOperandToolboxCtrl', function($timeout) {
         var ctrl = this;
 
-        // extend the operands in the toolbox by supplying them with a method
-        // that removes them from the toolbox, followed by a toolbox refresh
-        /* eslint-disable angular/controller-as-vm */
-        function OperandOptions(config) {
-            this.class = config.class;
-            this.label = config.label;
-        }
-
-        OperandOptions.prototype.removeOperand = function() {
-            ctrl.refresh(ctrl.getIndexOfOperand(this));
-        };
-        /* eslint-enable angular/controller-as-vm */
-
         ctrl.getIndexOfOperand = function(operand) {
             for (var i = 0; i < ctrl.operands.length; ++i) {
                 if (ctrl.operands[i].class === operand.class &&
@@ -31,19 +18,21 @@ angular.module('ngEquation')
 
         ctrl.refresh = function(operandIndex) {
             if (angular.isDefined(operandIndex) && operandIndex !== -1) {
-                var freshOperand = new OperandOptions(originalOperandsList[operandIndex]);
+                var freshOperand = angular.copy(originalOperandsList[operandIndex]);
                 ctrl.operands.splice(operandIndex, 1, freshOperand);
             } else {
                 ctrl.operands = [];
                 $timeout(function() {
-                    ctrl.operands = originalOperandsList.map(function(operand) {
-                        return new OperandOptions(operand);
-                    });
+                    ctrl.operands = angular.copy(originalOperandsList);
                 });
             }
         };
 
         ctrl.refresh();
+
+        ctrl.removeOperand = function(operand) {
+            ctrl.refresh(ctrl.getIndexOfOperand(operand));
+        };
     })
     .directive('expressionOperandToolbox', function($templateCache) {
         return {
