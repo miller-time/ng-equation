b365477bd9b79bd4acb8a59946ddbc9a034ac1f5
Russell Cousineau <russell.miller@rentrakmail.com>
2016-07-08 13:35:20 -0700

    remove now unused code that removed operands from the toolbox and refreshed

--- src/js/expression-operand-toolbox.js
+++ src/js/expression-operand-toolbox.js
@@ -1,39 +1,7 @@
 'use strict';
 
 angular.module('ngEquation')
-    .controller('ExpressionOperandToolboxCtrl', function($timeout) {
-        var ctrl = this;
-
-        ctrl.getIndexOfOperand = function(operand) {
-            for (var i = 0; i < ctrl.operands.length; ++i) {
-                if (ctrl.operands[i].class === operand.class &&
-                    ctrl.operands[i].label === operand.label) {
-                    return i;
-                }
-            }
-            return -1;
-        };
-
-        var originalOperandsList = angular.copy(ctrl.operands);
-
-        ctrl.refresh = function(operandIndex) {
-            if (angular.isDefined(operandIndex) && operandIndex !== -1) {
-                var freshOperand = angular.copy(originalOperandsList[operandIndex]);
-                ctrl.operands.splice(operandIndex, 1, freshOperand);
-            } else {
-                ctrl.operands = [];
-                $timeout(function() {
-                    ctrl.operands = angular.copy(originalOperandsList);
-                });
-            }
-        };
-
-        ctrl.refresh();
-
-        ctrl.removeOperand = function(operand) {
-            ctrl.refresh(ctrl.getIndexOfOperand(operand));
-        };
-    })
+    .controller('ExpressionOperandToolboxCtrl', function() {})
     .directive('expressionOperandToolbox', function($templateCache) {
         return {
             restrict: 'EA',
