18d2fd754719cec9f5de803f010f27545e270d2c
Russell Cousineau <russell.miller@rentrakmail.com>
2016-07-18 13:06:13 -0700

    equation component tests: add test for toolbox label transclusion

--- test/equation-spec.js
+++ test/equation-spec.js
@@ -2,6 +2,7 @@
 
 describe('equation directive', function() {
     var $scope,
+        element,
         controller,
         instantiate;
 
@@ -11,10 +12,11 @@ describe('equation directive', function() {
         $scope = $rootScope.$new();
         $scope.myOptions = {opt: 1};
 
-        instantiate = function() {
-            var element = angular.element(
+        instantiate = function(toolboxLabel) {
+            element = angular.element(
                 '<equation ' +
                     'equation-options="myOptions">' +
+                    (toolboxLabel || '') +
                 '</equation>'
             );
             $compile(element)($scope);
@@ -42,6 +44,30 @@ describe('equation directive', function() {
                 operator: 'AND',
                 children: []
             });
+        });
+
+        it('should contain the default toolbox label', function() {
+            var labelElem = element.find('.eq-toolbox-label');
+            expect(labelElem.length).toBe(1);
+            expect(labelElem.text().trim()).toEqual('Toolbox');
+        });
+    });
+
+    describe('with toolbox label transcluded', function() {
+        beforeEach(function() {
+            instantiate(
+                '<toolbox-label>' +
+                    '<span class="custom-label">Custom</span>' +
+                '</toolbox-label>'
+            );
+        });
+
+        it('should contain the custom label element that was transcluded', function() {
+            expect(element.find('.eq-toolbox-label').length).toBe(0);
+
+            var labelElem = element.find('.custom-label');
+            expect(labelElem.length).toBe(1);
+            expect(labelElem.text().trim()).toEqual('Custom');
         });
     });
 });
