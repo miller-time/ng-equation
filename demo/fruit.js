74adf20fdb4c45b9bb6ac12fbd8262161091b9a1
Russell Cousineau <russell.miller@rentrakmail.com>
2016-07-26 10:56:04 -0700

    update demo to select current fruit when re-opening edit dialog

--- demo/fruit.js
+++ demo/fruit.js
@@ -7,8 +7,8 @@ angular.module('exampleApp')
             return value.charAt(0).toUpperCase() + value.slice(1);
         };
     })
-    .factory('fruitOperand', function($uibModal, capitalize, fruits) {
-        var launchEditModal = function() {
+    .factory('fruitOperand', function($filter, $uibModal, capitalize, fruits) {
+        var launchEditModal = function(value) {
             var modalInstance = $uibModal.open({
                 size: 'sm',
                 templateUrl: 'templates/fruit-select-modal.html',
@@ -17,17 +17,30 @@ angular.module('exampleApp')
                         return {label: capitalize(fruit), value: fruit};
                     });
                     $scope.fruit = {
-                        selected: [{}]
+                        selected: []
                     };
+
+                    // select current fruit in dropdown
+                    var current = $filter('filter')($scope.fruits, function(fruit) {
+                        return fruit.value === value;
+                    });
+                    if (current[0]) {
+                        $scope.fruit.selected.push({obj: current[0]});
+                    } else {
+                        $scope.fruit.selected.push({});
+                    }
+
                     $scope.addAnother = function() {
                         $scope.fruit.selected.push({});
                     };
+
                     $scope.submit = function() {
                         var values = $scope.fruit.selected.map(function(fruit) {
                             return fruit.obj.value;
                         });
                         $uibModalInstance.close({value: values, addMultiple: true});
                     };
+
                     $scope.cancel = function() {
                         $uibModalInstance.dismiss();
                     };
@@ -42,8 +55,8 @@ angular.module('exampleApp')
                 return {
                     class: 'fruit',
                     typeLabel: 'Fruit',
-                    editMetadata: function() {
-                        return launchEditModal();
+                    editMetadata: function(operand) {
+                        return launchEditModal(operand.value);
                     },
                     getLabel: function(operand) {
                         return operand.value ? capitalize(operand.value) : 'N/A';
